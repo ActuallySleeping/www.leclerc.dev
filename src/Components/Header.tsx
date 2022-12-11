acc1b20a326a1bab52df97821b95606de9c44b9d
ActuallySleeping <leoleclercleo@gmail.com>
2022-12-11 00:01:48 +0000

    initial commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+    return (<header>
+        <div className="left">
+
+        </div>
+        <div className="right">
+
+        </div>
+    </header>
+)}
+
+export default Header
